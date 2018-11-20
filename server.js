e7a6eb0797d5e681a37f0210bad9d0e1dbcdfd32
chimingk <chikwan@ufl.edu>
2018-11-20 18:44:05 -0500

    Update server.js
    Result of test connection was "MongoDB connected..."

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const express = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -7,3 +7,22 @@ const app = express();
 // Middleware for BodyParser
 app.use(bodyParser.json());
 
+
+// Go create the config folder and keys.js and put the MongoDB URI there
+
+
+// Configure the DB
+const db = require('./config/keys').mongoURI;
+
+
+// Connect to MongoDB using mongoose
+mongoose
+ .connect(db)
+ .then( () => console.log('MongoDB Connected...') )
+ .catch( err => console.log(err) );
+
+
+// Create a variable for the port we're going to use for server deployment
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
