65a9206e815c0004e9fe55ee5d1ef0cf6b651039
chimingk <chikwan@ufl.edu>
2018-11-20 20:03:24 -0500

    Create Item.js
    Schema for the data objects we'll be using with the backend

---
+++ models/Item.js
@@ -0,0 +1,67 @@
+// Require mongoose
+const mongoose = require('mongoose');
+
+// Template schema
+const Schema = mongoose.Schema;
+
+
+// Create the Schema
+const ItemSchema = new Schema( {
+
+	incidentID: {
+		type: String,
+		required: false
+	},
+
+	incidentType: {
+		type: String,
+		required: true
+	},
+
+	incidentAddress: {
+		type: String,
+		required: false
+	},
+
+	incidentCoords: {
+		type: String,
+		required: true
+	},
+
+	incidentDate: {
+		type: Date,
+		required: true,
+		default: Date.now
+	},
+
+	incidentDescription: {
+		type: String,
+		required: false
+	},
+
+	incidentPictureURL: {
+		type: String,
+		required: false
+	},
+
+	incidentStatus: {
+		type: String,
+		required: true,
+		default: 'Reported'
+	},
+
+	managerNotes: {
+		type: String,
+		required: false
+	},
+
+	statusNotes: {
+		type: String,
+		required: false
+	}
+
+});
+
+
+// Export this model so that we can bring it into other files
+module.exports = Item = mongoose.model('item', ItemSchema);
