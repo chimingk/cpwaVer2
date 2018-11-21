a023440c35220ff9c102b90ee406afb9de610f29
chimingk <chikwan@ufl.edu>
2018-11-21 04:31:51 -0500

    Update server.js
    so that server.js can find items.js for the api routes, added line const items = require('./routes/api/items');

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+
+//so that server.js can find items.js for the api routes
+const items = require('./routes/api/items');
+
+
 const app = express();
+
+
 
 // Middleware for BodyParser
 app.use(bodyParser.json());
@@ -23,6 +30,25 @@ mongoose
 
 
 // Create a variable for the port we're going to use for server deployment
+//		process.env.PORT is for deployment to Heroku purposes
 const port = process.env.PORT || 5000;
 
+
+
+// Now we want to be able to interact with our API
+// (1) Get | (2) Post | (3) Delete
+// Use Routes
+//    Anything that goes to /api/items should refer to the items variable defined earlier
+app.use('/api/items', items)
+
+
+
+
+
+
+
+// This is just a check to see if it successfully connected to the mongoDB
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
+
