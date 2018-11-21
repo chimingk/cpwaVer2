52f57847e0c19466d24e685207e180ee747811c6
chimingk <chikwan@ufl.edu>
2018-11-21 08:51:34 -0500

    Update Item.js
    I think for the incident date, we only need to get the date part of it in YYYYMMDD format. This will be easier to sort in future instances.
    
    If we want to revert back to the full date, we can update the schema in the future to
    
    incidentDate: {type: Date, required: true, default: Date.now}

--- models/Item.js
+++ models/Item.js
@@ -5,14 +5,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
+// Just getting the date without time
+var dateEntered = new Date(Date.now());
+var yyyy = dateEntered.getFullYear().toString();
+var mm = ( dateEntered.getMonth() + 1 ) .toString();
+var dd = dateEntered.getDate().toString();
+var yyyymmdd = (yyyy.concat(mm).concat(dd));
+
+
+
 // Create the Schema
 const ItemSchema = new Schema( {
 
-	incidentID:            { type: String, required: false  },
+	incidentID:            { type: String, required: false },
 	incidentType:          { type: String, required: true   },
 	incidentAddress:       { type: String, required: false  },
 	incidentCoords:        { type: String, required: true   },
-	incidentDate:          { type: Date,   required: true,  default: Date.now},
+	incidentDate:          { type: String, required: true,  default: yyyymmdd },
 	incidentDescription:   { type: String, required: false	},
 	incidentPictureURL:    { type: String, required: false 	},
 	incidentStatus:        { type: String, required: true,  default: 'Reported'},
