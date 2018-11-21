217dff9b457de39f2f77e2cc354a3534b5a00069
chimingk <chikwan@ufl.edu>
2018-11-21 04:30:20 -0500

    Create items.js
    using express as the middleware, we're going to get all the items that reach the end point api/items and then sort them descending by date and return them as json objects

---
+++ routes/api/items.js
@@ -0,0 +1,42 @@
+
+// This is where the get, post, and delete api routes will be
+//  however, inorder for server.js to know where this is
+//  need to create
+//			const items = require ('./routes/api/items')
+//  		app.use('/api/items', items)
+// then comeback here to create the middleware
+
+
+//Middleware function
+const express = require('express');
+
+
+// Router is part of the express object here
+const router = express.Router();
+
+
+// Bring in the Item Model so that we can do stuff like item.find or item.save
+const Item = require('../../models/Item');
+
+
+// @		GET api/items
+// @desc	Get All Items
+// @access	Public
+// '/' parameter represents the api/items endpoint
+router.get('/', (req, res) => {
+
+	Item.find()
+	 .sort({ date: -1} )
+	 .then(items => res.json(items))
+
+});
+
+
+
+
+
+
+// Important - Export this so that other files can read what in here
+module.exports = router;
+
+
